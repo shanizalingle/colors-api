88d7e8ca497f966f6944384981ea57325bdd3621
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-07 16:51:19 -0700

    add styling
    Co-authored-by: Tiberius Lockett <Tiberiusloc@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -24,9 +24,6 @@ function getAllCharacters() {
   });
 }
 
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
 function getRandomRick() {
 const ricks = [1,8,15,19,22,48,56,69,72,74,78,82,86,103,119,135,164,165,187,215,218,220,231,265,267,274,278,281,283,284,285,286,287,288,289,290,291,292,293,294,299,322,328,330,345,349,353,361,378,380,381,385,389,434,461,462,463,464,465,466,472,477,478,479,481,482,483,484,485,486,487,488,489,497,498,501,503,504,507,508,516,517,631,638,687,694,698,701,702,707,713,760,769,770,772,773,779,781,783,784,810,814,815,816,817,818,819];
 let random = Math.floor(Math.random() * ricks.length);
@@ -39,8 +36,11 @@ function printCharacter(character) {
   console.log(character);
   let randomRick = getRandomRick();
   console.log(randomRick);
-  document.querySelector("#showRick").innerText = `Your character is ${character[0].results[randomRick].name}`
-  //document.querySelector('#charimg').src = `${character["results"][0]['image']}`
+  document.querySelector("#showRick").innerText = `Your character is ${character[0][randomRick].name}`
+  document.querySelector('#charimg').src = `${character[0][randomRick]['image']}`
+  document.querySelector("#statusRick").innerText = `${character[0][randomRick].status}`
+  document.querySelector("#speciesRick").innerText = `${character[0][randomRick].species}`
+  document.querySelector("#originRick").innerText = `${character[0][randomRick].origin.name}`
 }
 
 function printColor(color) {
