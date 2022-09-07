634283f0fc534d6fa085bef90d26b1c876c329cd
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-07 10:14:03 -0700

    make api display random color
    Co-authored-by: Tiberius Lockett <tlockett680@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import RandomColor from './js/colors-api.js';
+
+//Business Logic
+
+export function getRandomColor() {
+  let promise = RandomColor.getRandomColor();
+  promise.then(function(color) {
+    console.log(color);
+    printColor(color);
+  }, function (error) {
+    printError(error);
+  });
+}
+
+//UI Logic
+
+function printColor(color) {
+  document.querySelector("#showResponse").innerText = `Your random color is ${color[0]['hex']} ${color[0][`rgb`]} ${color[0][`hsl`]}`;
+}
+
+function printError(error) {
+  document.querySelector("#showResponse").innerText = `There was an error processing the color data for ${error}: ${error.status} ${error.statusText}: ${error.message};`
+}
+
+function handleSubmission(event) {
+  event.preventDefault();
+  getRandomColor();
+}
+
+window.addEventListener("load", function() {
+  this.document.querySelector("#submitform").addEventListener("submit", handleSubmission);
+})
---
+++ src/js/colors-api.js
@@ -0,0 +1,20 @@
+//import { getRandomColor } from "./index.js";
+
+export default class RandomColor {
+  static getRandomColor() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = 'https://x-colors.herokuapp.com/api/random';
+      request.addEventListener('loadend', function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response]);
+        } else {
+          reject([this, response]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
